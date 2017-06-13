81a63c9b78fa6e9aab551a97f5f2e688596719b0
Areg Sarkissian <aregsar@gmail.com>
2017-06-12 19:12:01 -0700

    applied feature flag to add gig link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                        {{ config('app.name', 'Gigtown') }}
                     </a>
                 </div>
 
@@ -46,7 +46,9 @@
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
-                            <li><a href="{{ route('gig.showAddForm') }}">Add Gig</a></li>
+                            @if(config('app.features.gig'))
+                                <li><a href="{{ route('gig.showAddForm') }}">Add Gig</a></li>
+                            @endif
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
