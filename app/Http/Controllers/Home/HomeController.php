c4cab92d1706021c4614c7a150000f04c28cd74a
Areg Sarkissian <aregsar@gmail.com>
2017-06-25 10:40:12 -0700

    moved homecontroller

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -1,17 +1,12 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Home;
+use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-
-    // public function __construct()
-    // {
-    //     $this->middleware('auth');
-    // }
-
     /**
      * Show the application dashboard.
      *
--- routes/web.php
+++ routes/web.php
@@ -38,7 +38,7 @@
 Auth::routes();
 
 
-Route::get('/', 'HomeController@index')->name('home');
+Route::get('/', 'home\HomeController@index')->name('home');
 
 
 /*
