d0efd26be532f6d181357102cf577f86f36cdc25
Areg Sarkissian <aregsar@gmail.com>
2017-06-25 10:18:23 -0700

    removed home route

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 /*
  * From Illuminate/Routing/Router.php
@@ -40,7 +38,7 @@ Route::get('/', function () {
 Auth::routes();
 
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 
 
 /*
